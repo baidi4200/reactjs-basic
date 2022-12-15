8adaa25c03963bf556e17a49ae85540628a227d9
baidi4200 <obaidobaid345@gmail.com>
2022-12-15 14:51:45 +0500

    rebasing-round1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
-import { Todo } from './components/Round1/Todo';
 
 function App() {
   return (
     <div className="App">
 
       <h1>My Todos</h1>
-      <Todo text='Learn React' />
 
 
     </div>
