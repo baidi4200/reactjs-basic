8e4420257a74c680b42b909a5d41269e22199b15
baidi4200 <obaidobaid345@gmail.com>
2022-12-20 14:55:39 +0500

    getting user inputa and handling form submission

--- src/pages/NewMeetups.js
+++ src/pages/NewMeetups.js
@@ -1,7 +1,10 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
+
 function NewMeetups() {
   return (
     <section>
       <h1>Add NewMeetups</h1>
+      <NewMeetupForm />
     </section>
   );
 }
