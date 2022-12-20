a35fb44e1f179d369ed12513ac5f8ae733894644
baidi4200 <obaidobaid345@gmail.com>
2022-12-20 16:54:04 +0500

    added useEffect and key data with firbase data fetching

--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,4 +1,5 @@
 import MeetupList from "../components/meetups/MeetupList";
+import { useEffect, useState } from "react";
 
 const DUMMY_MEETUPS = [
   {
@@ -20,10 +21,42 @@ const DUMMY_MEETUPS = [
 ];
 
 function AllMeetups() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [meetupsdata, setmeetupsdata] = useState([]);
+  useEffect(() => {
+    setIsLoading(true);
+    fetch("https://reactjs-37c15-default-rtdb.firebaseio.com/meetups.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setmeetupsdata(meetups);
+        console.log(data);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
-      <MeetupList meetups={DUMMY_MEETUPS} />
+      <MeetupList meetups={meetupsdata} />
     </section>
   );
 }
